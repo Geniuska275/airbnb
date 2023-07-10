a28cafa4958fb7a5db4b160a22aef6dc259e23ca
geniuska <aigojie2020@gmail.com>
2023-07-10 19:11:21 +0100

    hello

--- src/components/toggler.jsx
+++ src/components/toggler.jsx
@@ -3,10 +3,10 @@ import SwitchDemo from "./toggle";
 export default function Toggler({ setTaxes }) {
   return (
     <div className="flex justify-center items-center  p-4">
-      <div className="flex justify-between  items-center rounded-md border border-gray-100 p-6 gap-7 w-[600px] ">
-        <div className="flex gap-2 items-center">
+      <div className="flex justify-between  items-center rounded-lg border border-gray-200 p-6 gap-7 w-[600px] ">
+        <div className=" md:flex gap-2 md:items-center">
           <h2 className="font-bold text-black">Display Total Price</h2>
-          <h2 className="text-gray-200  border-l-2 p-1">
+          <h2 className="font-bold text-gray-500 md:border-l-2 md:p-1">
             Include all fees,before taxes
           </h2>
         </div>
