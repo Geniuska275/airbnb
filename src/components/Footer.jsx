980a82326cc2b9add408bee7423f4b836cde2638
geniuska <aigojie2020@gmail.com>
2023-07-11 07:34:34 +0100

    hello

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { CgProfile } from "react-icons/cg";
 export default function Footer() {
   return (
     <div className="flex justify-center items-center shadow mt-6 p-3 sticky bottom-0 bg-white">
-      <div className="mr-[100px]">
+      <div className="mr-[50px]">
         <FiSearch className="text-[#b6b6b6] ml-3 text-2xl " />
         <h5 className="text-[#b6b6b6] font-Poppins">Explore</h5>
       </div>
@@ -13,7 +13,7 @@ export default function Footer() {
         <AiOutlineHeart className="text-[#b6b6b6] ml-3 text-2xl" />
         <h4 className="text-[#b6b6b6]">Wishlist</h4>
       </div>
-      <div className=" ml-[100px]">
+      <div className=" ml-[50px]">
         <CgProfile className="text-[#b6b6b6] ml-3 text-2xl" />
         <h4 className="text-[#b6b6b6]">Log in</h4>
       </div>
